d84f38f237523b81abf6c1945a9a66128e61fc9e
unknown <xlongclaw@gmail.com>
2024-04-09 13:40:02 +0530

    feat: add data to services tiles

---
+++ src/containers/services-section/SERVICES.ts
@@ -0,0 +1,80 @@
+import flowChart from "./svgs/flow-chart.svg";
+import paper from "./svgs/paper.svg";
+import stockGrowth from "./svgs/stock-growth.svg";
+
+const SERVICES = [
+  {
+    title: "Eazy Trading",
+    icon: stockGrowth,
+    keyPoints: [
+      "One Tap to Trade",
+      "Intuitive Platform design",
+      "Dedicated support team",
+    ],
+  },
+  {
+    title: "Algorithm Trading",
+    icon: flowChart,
+    keyPoints: [
+      "One Tap to Trade",
+      "Intuitive Platform design",
+      "Dedicated support team",
+    ],
+  },
+  {
+    title: "Paper Trading",
+    icon: paper,
+    keyPoints: [
+      "One Tap to Trade",
+      "Intuitive Platform design",
+      "Dedicated support team",
+    ],
+  },
+  {
+    title: "Eazy Trading",
+    icon: stockGrowth,
+    keyPoints: [
+      "One Tap to Trade",
+      "Intuitive Platform design",
+      "Dedicated support team",
+    ],
+  },
+  {
+    title: "Algorithm Trading",
+    icon: flowChart,
+    keyPoints: [
+      "One Tap to Trade",
+      "Intuitive Platform design",
+      "Dedicated support team",
+    ],
+  },
+  {
+    title: "Paper Trading",
+    icon: paper,
+    keyPoints: [
+      "One Tap to Trade",
+      "Intuitive Platform design",
+      "Dedicated support team",
+    ],
+  },
+  {
+    title: "Algorithm Trading",
+    icon: flowChart,
+    keyPoints: [
+      "One Tap to Trade",
+      "Intuitive Platform design",
+      "Dedicated support team",
+    ],
+  },
+  {
+    title: "Paper Trading",
+    icon: paper,
+    keyPoints: [
+      "One Tap to Trade",
+      "Intuitive Platform design",
+      "Dedicated support team",
+    ],
+  },
+];
+
+export default SERVICES;
--- src/containers/services-section/service-tile/index.tsx
+++ src/containers/services-section/service-tile/index.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-export default function ServiceTile() {
+interface IProps {
+  icon: any;
+  title: string;
+  keyPoints: string[];
+}
+
+export default function ServiceTile(props: IProps) {
   return (
-    <div className='w-56 h-56 rounded-3xl glass3'>
-      
+    <div className="w-56 h-56 rounded-3xl glass3 p-4">
+      <Image src={props.icon} alt="" />
+      <h6>{props.title}</h6>
+      <div className="mt-2">
+        {props.keyPoints.map((point) => (
+          <p className="text-sm my-1">{point}</p>
+        ))}
+      </div>
+      <h6></h6>
     </div>
-  )
+  );
 }
